db943cb7a9bcd91c6a9cda82aa3c416f3f8a2460
Dhruvisha Pandya <pandyadhruvisha@gmail.com>
2025-04-16 18:06:08 +0530

    removed unnecessary folders and updated code

--- backend/utils/firebase_admin.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, auth, firestore
-
-# Download your Firebase service account key:
-# Firebase Console → Project Settings → Service Accounts → Generate Key
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()  # Firestore DB
