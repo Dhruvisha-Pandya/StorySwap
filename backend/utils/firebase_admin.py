dd18e8d842b5c9cce308fa6200ab7dc0cc088813
Dhruvisha Pandya <pandyadhruvisha@gmail.com>
2025-11-10 12:49:45 +0530

    ready to deploy

--- backend/utils/firebase_admin.py
+++ backend/utils/firebase_admin.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
-# Download your Firebase service account key:
+# Firebase service account key:
 # Firebase Console → Project Settings → Service Accounts → Generate Key
 cred = credentials.Certificate("path/to/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
